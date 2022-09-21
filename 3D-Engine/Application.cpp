2612b42173222b94d797e2b7d60bc0f17f4d87a3
Akage369 <xaviercasado369@gmail.com>
2022-09-21 22:57:21 +0200

    Code cleaning

--- 3D-Engine/Application.cpp
+++ 3D-Engine/Application.cpp
@@ -8,8 +8,6 @@ Application::Application()
 	scene_intro = new ModuleSceneIntro(this);
 	renderer3D = new ModuleRenderer3D(this);
 	camera = new ModuleCamera3D(this);
-	physics = new ModulePhysics3D(this);
-	player = new ModulePlayer(this);
 
 	// The order of calls is very important!
 	// Modules will Init() Start() and Update in this order
@@ -20,11 +18,9 @@ Application::Application()
 	AddModule(camera);
 	AddModule(input);
 	AddModule(audio);
-	AddModule(physics);
 	
 	// Scenes
 	AddModule(scene_intro);
-	AddModule(player);
 
 	// Renderer last!
 	AddModule(renderer3D);
--- 3D-Engine/ModuleSceneIntro.cpp
+++ 3D-Engine/ModuleSceneIntro.cpp
@@ -2,7 +2,6 @@
 #include "Application.h"
 #include "ModuleSceneIntro.h"
 #include "Primitive.h"
-#include "PhysBody3D.h"
 
 ModuleSceneIntro::ModuleSceneIntro(Application* app, bool start_enabled) : Module(app, start_enabled)
 {
@@ -39,9 +38,5 @@ update_status ModuleSceneIntro::Update(float dt)
 	p.Render();
 
 	return UPDATE_CONTINUE;
-}
-
-void ModuleSceneIntro::OnCollision(PhysBody3D* body1, PhysBody3D* body2)
-{
 }
 
