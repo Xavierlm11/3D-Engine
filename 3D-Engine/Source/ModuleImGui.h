ae539603cf9730f0f55873e5609b32149e0ea9fb
Akage369 <xaviercasado369@gmail.com>
2022-09-28 23:22:56 +0200

    ImGui Demo Windows Intercatuable
    si

--- 3D-Engine/Source/ModuleImGui.h
+++ 3D-Engine/Source/ModuleImGui.h
@@ -1,6 +1,17 @@
 #pragma once
 #include "Module.h"
 #include "Globals.h"
+#include "ImGui/imgui.h"
+#include "ImGui/imgui_impl_sdl.h"
+#include "ImGui/imgui_impl_opengl3.h"
+#include <stdio.h>
+#include "SDL/include/SDL.h"
+
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <SDL/include/SDL_opengles2.h>
+#else
+#include <SDL/include/SDL_opengl.h>
+#endif
 
 
 class ModuleImGui : public Module
@@ -13,5 +24,13 @@ public:
 	bool Init();
 	update_status Update(float dt);
 	bool CleanUp();
+
+	// Our state
+	bool show_demo_window = true;
+	bool show_another_window = false;
+	ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
+	//ImGuiIO io;
+	SDL_GLContext gl_context;
 
 };
