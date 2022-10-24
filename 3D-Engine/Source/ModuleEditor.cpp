a27bb4979cc5361a240e8a0385839c91c5b916e6
Xavierlm11 <xaviloma11@gmail.com>
2022-10-24 11:11:57 +0200

    docking

--- 3D-Engine/Source/ModuleEditor.cpp
+++ 3D-Engine/Source/ModuleEditor.cpp
@@ -70,8 +70,16 @@ bool ModuleEditor::Init()
 	ImGuiIO& io = ImGui::GetIO(); (void)io;
 	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
 	//io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
-
+	io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;           // Enable Docking
+	io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;         // Enable Multi-Viewport / Platform Windows
 	// Setup Dear ImGui style
+
+	ImGuiStyle& style = ImGui::GetStyle();
+	if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable)
+	{
+		style.WindowRounding = 0.0f;
+		style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+	}
 	ImGui::StyleColorsDark();
 	//ImGui::StyleColorsClassic();
 
