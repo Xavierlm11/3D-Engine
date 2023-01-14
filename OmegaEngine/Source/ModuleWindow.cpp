324e2397b2b7c73c7ceb7d2d5549c72844b33e89
Xavierlm11 <xaviloma11@gmail.com>
2023-01-14 16:57:27 +0100

    Update ModuleWindow.cpp

--- OmegaEngine/Source/ModuleWindow.cpp
+++ OmegaEngine/Source/ModuleWindow.cpp
@@ -14,7 +14,7 @@ ModuleWindow::ModuleWindow(Application* app, bool start_enabled) : Module(app, s
 ModuleWindow::~ModuleWindow()
 {
 }
-
+ 
 // Called before render is available
 bool ModuleWindow::Init()
 {
